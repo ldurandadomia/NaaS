8481bff758aae85e7ae7a6bcfa7fddaec17751c8
ldurandadomia <ldurandadomia@users.noreply.github.com>
2017-07-04 17:14:52 +0200

    added pylint

--- authapp.py
+++
@@ -1,5 +0,0 @@
-#!flask/bin/python
-from authapp import app
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=7000)
--- routeapp.py
+++
@@ -1,5 +0,0 @@
-#!flask/bin/python
-from routeapp import app
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=9000)
